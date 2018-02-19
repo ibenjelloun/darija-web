99d45d07c132bacda4f4ba9b37884a105a22924d
LAPTOP-D8PKKVPH\Ismail <ismail.benjelloun@gmail.com>
2018-02-19 21:29:49 +0100

    Backup environment files to fix travis build

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'TODO',
+    authDomain: 'TODO',
+    databaseURL: 'TODO',
+    projectId: 'TODO',
+    storageBucket: 'TODO',
+    messagingSenderId: 'TODO'
+  }
+};
