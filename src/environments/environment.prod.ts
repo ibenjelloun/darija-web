ef3ac64fe2fb80b678d5ca7f0f4e39a19ba82ec2
LAPTOP-D8PKKVPH\Ismail <ismail.benjelloun@gmail.com>
2018-02-20 18:55:51 +0100

    Travis firebase deploy fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
   production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCOC7DPCFQ2gWiV3D0sYidQngVcAXMBMlA',
+    authDomain: 'darija-web.firebaseapp.com',
+    databaseURL: 'https://darija-web.firebaseio.com',
+    projectId: 'darija-web',
+    storageBucket: 'darija-web.appspot.com',
+    messagingSenderId: '1082843981941'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,4 +5,12 @@
 
 export const environment = {
   production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCOC7DPCFQ2gWiV3D0sYidQngVcAXMBMlA',
+    authDomain: 'darija-web.firebaseapp.com',
+    databaseURL: 'https://darija-web.firebaseio.com',
+    projectId: 'darija-web',
+    storageBucket: 'darija-web.appspot.com',
+    messagingSenderId: '1082843981941'
+  }
 };
