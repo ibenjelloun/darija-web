f885f5c58b9d679c341b69aff64239f998f3435e
LAPTOP-D8PKKVPH\Ismail <ismail.benjelloun@gmail.com>
2018-04-11 21:43:41 +0200

    Fix profil component and service

--- src/app/profil/profil.component.ts
+++ src/app/profil/profil.component.ts
@@ -1,19 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProfilService } from './services/profil.service';
 
 @Component({
   selector: 'dar-profil',
   template:
   `
-    <dar-profil-details [user]="(data.user | async)" [profil]="(data.profil | async)"></dar-profil-details>
+    <dar-profil-details [user]="user$ | async" [profil]="profil$ | async"></dar-profil-details>
   `
 })
-export class ProfilComponent implements OnInit {
-  data;
+export class ProfilComponent implements OnInit, OnDestroy {
+  profil$;
+  user$;
+
+  sub;
 
   constructor(private _profileService: ProfilService) {}
 
   ngOnInit() {
-    this.data = this._profileService.getProfilInfo();
+    this.user$ = this._profileService.getUser();
+    this.sub = this.user$.subscribe(user => this.profil$ = this._profileService.getProfil(user.uid));
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 }
--- src/app/profil/services/profil.service.ts
+++ src/app/profil/services/profil.service.ts
@@ -1,31 +1,28 @@
-import { Injectable, OnDestroy } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { AuthService } from '../../core/services/auth.service';
 import { Subscription } from 'rxjs/Subscription';
-import { tap, first } from 'rxjs/operators';
+import { tap, first, switchMap, filter } from 'rxjs/operators';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from '@firebase/util';
 
 @Injectable()
-export class ProfilService implements OnDestroy {
-  authSubscription: Subscription;
-  profil = new BehaviorSubject({});
+export class ProfilService {
 
-  constructor(private _authService: AuthService, private afs: AngularFirestore) {
-    this.authSubscription = this._authService.getUser().subscribe(user =>
-      this.afs
+  constructor(
+    private _authService: AuthService,
+    private afs: AngularFirestore
+  ) {
+  }
+
+  getProfil(uid) {
+    return this.afs
         .collection<any>('users')
-        .doc(user.uid)
-        .valueChanges()
-        .pipe(first())
-        .subscribe(profil => this.profil.next(profil))
-    );
+        .doc(uid)
+        .valueChanges();
   }
 
-  getProfilInfo() {
-    return { user: this._authService.getUser(), profil: this.profil };
-  }
-
-  ngOnDestroy() {
-    this.authSubscription.unsubscribe();
+  getUser() {
+    return this._authService.getUser();
   }
 }
