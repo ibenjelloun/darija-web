e0337446aaef669b0a43bb7a4300ad39ffd3353b
LAPTOP-D8PKKVPH\Ismail <ismail.benjelloun@gmail.com>
2018-04-23 22:06:57 +0200

    Functions fix

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -4,10 +4,7 @@ const functions = require("firebase-functions");
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-exports.computeRank = functions.https.onRequest((request, response) => {
+exports.helloWorld = functions.https.onRequest((request, response) => {
     response.send("Hello from Firebase!");
 });
 //# sourceMappingURL=index.js.map
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,11 +3,6 @@ import * as functions from 'firebase-functions';
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-
-
-export const computeRank = functions.https.onRequest((request, response) => {
+export const helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
